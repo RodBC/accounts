468a2747265abfc2d5b683d34b84195b0c1cf72f
RodBC <rbc6@cin.ufpe.br>
2022-07-29 10:39:07 -0300

    creating user's entity

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { AppDataSource } from 'src/data-source';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      return await AppDataSource.createQueryBuilder()
+        .insert()
+        .into(User)
+        .values(createUserDto)
+        .execute();
+    } catch(err){
+      console.log(err)
+      return HttpStatus.INTERNAL_SERVER_ERROR
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    try {
+      return await AppDataSource.createQueryBuilder()
+        .select('u')
+        .from(User, 'o')
+        .getMany();
+    } catch (err){
+      console.log(err)
+      return HttpStatus.INTERNAL_SERVER_ERROR
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async remove(id: number) {
+    try {
+      return await AppDataSource.createQueryBuilder()
+        .delete()
+        .from(User)
+        .where('id=:taskID', {taskID: id})
+        .execute()
+    } catch(err){
+      console.log(err)
+      return HttpStatus.INTERNAL_SERVER_ERROR
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
+
 }
